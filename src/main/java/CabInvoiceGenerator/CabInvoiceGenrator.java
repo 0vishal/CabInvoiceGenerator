7f8f3dd16a7723d8aaeeb2f50a2689da01d55966
0vishal <vishalsalaskar72@gmail.com>
2021-03-24 20:00:20 +0530

    Multiple Fare

--- src/main/java/CabInvoiceGenerator/CabInvoiceGenrator.java
+++ src/main/java/CabInvoiceGenerator/CabInvoiceGenrator.java
@@ -1,17 +1,27 @@
 package CabInvoiceGenerator;
 
+import java.util.Arrays;
+
 class CabInvoiceGenerator {
         private final int COST_PER_KM = 10;
         private final int COST_PER_MINUTE = 1;
         private final double MINIMUM_FARE = 5.0;
 
-        public double calculateFare(double distance, int time) {
+        public double calculateFare( double distance, int time) {
             double totalFare;
             totalFare = distance * COST_PER_KM + time * COST_PER_MINUTE;
             if (totalFare < MINIMUM_FARE)
                 totalFare = MINIMUM_FARE;
 
             return totalFare;
+        }
+
+        public double MultipleFare(Rides[] rides) {
+        double totalFare = 0.0;
+        for (Rides ride : rides)
+            totalFare = totalFare + this.calculateFare(ride.distance,ride.time);
+
+        return totalFare;
         }
     }
 
--- src/test/java/CabInvoiceGenerator/CabInvoiceGeneratorTest.java
+++ src/test/java/CabInvoiceGenerator/CabInvoiceGeneratorTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test;
             cabInvoiceGenerator=new CabInvoiceGenerator();
         }
         @Test
-        void calculateFareTrueTest() {
+        void CalculateFareTrueTest() {
             double totalFare = cabInvoiceGenerator.calculateFare(1.0, 5);
             Assertions.assertEquals(15.0, totalFare, 0.0);
         }
@@ -23,4 +23,20 @@ import org.junit.jupiter.api.Test;
             double totalFare = cabInvoiceGenerator.calculateFare(1.0, 5);
             Assertions.assertEquals(5, totalFare, 0);
         }
+
+        @Test
+        public void InvoiceGeneratorMinimumFare() {
+            double totalFare = cabInvoiceGenerator.calculateFare(0.2, 2);
+            Assertions.assertEquals(5, totalFare, 0.0);
+        }
+
+        @Test
+        public void MultipleRidesTotalFare() {
+            Rides[] rides = { new Rides(1.0,5),
+                    new Rides(0.2,2)};
+
+            double totalFare = cabInvoiceGenerator.MultipleFare(rides);
+            Assertions.assertEquals(20, totalFare, 0.0);
+        }
+
     }
